cefdbd234bd640bf37307588d91703a22714a54a
Mustufa-Ismail <mustufaismail7942@gmail.com>
2025-09-19 19:31:21 +0500

    task 4 and 5 uploaded

--- Lab_4/Task-4.cpp
+++ Lab_4/Task-4.cpp
@@ -1,4 +1,73 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int interpolationSearch(int arr[], int size, int x)
+{
+    int low = 0;
+    int high = size - 1;
+    while (low <= high && x >= arr[low] && x <= arr[high])
+    {
+        if (low == high)
+        {
+            if (arr[low] == x)
+            {
+                return low;
+            }
+            return -1;
+        }
+
+        int pos = low + ((x - arr[low]) * (high - low)) / (arr[high] - arr[low]);
+        if (arr[pos] == x)
+        {
+            return pos;
+        }
+        else if (arr[pos] < x)
+        {
+            low = pos + 1;
+        }
+        else
+        {
+            high = pos - 1;
+        }
+    }
+    return -1;
+}
+
+void shellSort(int arr[], int size)
+{
+    for (int i = size / 2; i > 0; i /= 2)
+    {
+        for (int j = i; j < size; j++)
+        {
+            int temp = arr[j];
+            int res = j;
+
+            while (res >= i && arr[res - i] > temp)
+            {
+                arr[res] = arr[res - i];
+                res -= i;
+            }
+            arr[res] = temp;
+        }
+    }
+}
+
+void display(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[10] = {1, 55, 11, 93, 00, 45, 39, 41, 77, 2};
+
+    shellSort(arr, 10);
+    display(arr, 10);
+    int pos = interpolationSearch(arr, 10, 45);
+
+    cout << "Index is :" << pos + 1;
+}
--- Lab_4/Task-5.cpp
+++ Lab_4/Task-5.cpp
@@ -1,4 +1,86 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int binarySearch(int arr[], int size, int x)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (arr[mid] > x)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+void shellSort(int arr[], int size)
+{
+    for (int i = size / 2; i > 0; i /= 2)
+    {
+        for (int j = i; j < size; j++)
+        {
+            int temp = arr[j];
+            int res = j;
+
+            while (res >= i && arr[res - i] > temp)
+            {
+                arr[res] = arr[res - i];
+                res -= i;
+            }
+            arr[res] = temp;
+        }
+    }
+}
+
+void display(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int *arr = new int[size];
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    shellSort(arr, size);
+
+    display(arr, size);
+
+    int val;
+    cout << "Enter the value to search for: ";
+    cin >> val;
+
+    int result = binarySearch(arr,size,val);
+    if (result == -1)
+    {
+        cout << "Value not found";
+    }
+    else
+    {
+        cout << val << " is at index: " << result+1;
+    }
+}
